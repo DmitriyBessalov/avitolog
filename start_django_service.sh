9fb8ba0ae8811d671a1bb06dfe94f756b3210b09
dmitriy <dmitriy@bessalov.ru>
2020-06-20 17:15:09 +0300

    -

--- start_django_service.sh
+++ start_django_service.sh
@@ -1,4 +1,6 @@
 #!bin/bash
 cd /home/www/projects/avitolog
 source ./venv/bin/activate
-exec daphne -b 0.0.0.0 -p 8000 avitolog.asgi:application
+
+python3 manage.py runserver 0.0.0.0:8000
+# exec daphne -b 0.0.0.0 -p 8000 avitolog.asgi:application
